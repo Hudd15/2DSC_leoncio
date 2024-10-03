1f7c367f0d68684e6e31f1c00383e9c02953805f
Hudd15 <hudsondiasdelima15@gmail.com>
2024-10-03 15:23:46 +0000

    iniciando o cógido Javascript

---
+++ calculadora/js/script.js
@@ -0,0 +1,14 @@
+//FAZ REFERÊNCIA AOS ELEMENTOS DOM 
+const numero1 = document.querySelector('#numero1')
+const numero2 = document.querySelector('#numero2')
+const btnCalcular = document.querySelector('#btn-calcular')
+alert(numero1)
+const divisao = document.querySelector('.divisao')
+const adicao = document.querySelector('.adicao')
+const subtracao = document.querySelector('.subtracao')
+const multiplicacao = document.querySelector('.multiplicacao')
+alert(adicao)
+// CRIA A FUNÇÃO CALCULAR
+
+
+//ADICIONA ESCUTAR DE EVENTOS
